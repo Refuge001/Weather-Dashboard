4390d7c95a2dd97f827dee19d3ab46e614f25261
Will Utterback <uwill08@gmail.com>
2020-12-27 22:34:44 -0600

    Removed secret stuff

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// blank
-//api.openweathermap.org/data/2.5/forecast?id=524901&appid=f094b3d4247e89d90ebcf38a7e5d3caa
-
-// city/state
-//api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid=f094b3d4247e89d90ebcf38a7e5d3caa
-
-// country/state/city
-// api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid=f094b3d4247e89d90ebcf38a7e5d3caa
