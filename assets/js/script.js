e4769c844127fc7706d7c97dee3bc0762dc20232
Will Utterback <uwill08@gmail.com>
2020-12-28 22:32:05 -0600

    Code works, forced down the multiple function pathway

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
 var APIkey = "f094b3d4247e89d90ebcf38a7e5d3caa";
 
-// $(document).ready(function () {
-//   if (localStorage.getItem("lastCity") === null) {
-//     alert('localstorage empty');
-//   } else {
-//     citySearch = localStorage.getItem("lastCity");
-//     searchFunction(citySearch);
-//   }
-// });
+//Search if localStorage has a saved search in it, pass citySearch to defaultFunction
+$(document).ready(function () {
+  if (localStorage.getItem("lastCity") === null) {
+    alert('localstorage empty');
+  } else {
+    citySearch = localStorage.getItem("lastCity");
+    defaultFunction(citySearch);
+  }
+});
 
-function searchFunction() {
-  var citySearch = $('.buttonInput').prev().val();
+// runs query and displays search from localStorage (try to collapse into searchFunction)
+function defaultFunction(citySearch) {
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIkey;
   var queryForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&cnt=40&appid=" + APIkey;
 
@@ -27,6 +28,63 @@ function searchFunction() {
     lonQuery = (response.coord.lon);
     latQuery = (response.coord.lat);
     uvFunction(lonQuery, latQuery);
+    localStorage.setItem('lastCity', `${response.name}`);
+  });
+
+  function uvFunction(lonQuery, latQuery) {
+
+    var queryUV = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latQuery + "&lon=" + lonQuery + "&exclude=minutely,hourly,alerts,daily&appid=" + APIkey;
+    $.ajax({
+      url: queryUV,
+      method: "GET"
+    }).then(function (response) {
+      $('.uv').text(`UV Index: ${response.current.uvi}`)
+    });
+  }
+
+  $.ajax({
+    url: queryForecast,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+
+    $(".fiveday").each(function () {
+      var position = $(this).attr('id'); // ID in increments of 8: 8 * 3 = 24 hours (3 hour intervals)
+      var day = (response.list[position].dt_txt) // Day inside response
+      var iconID = (response.list[position].weather[0].icon) // find icon ID for website URL lookup
+      var weatherIcon = "http://openweathermap.org/img/w/" + iconID + ".png"; // weather icons are located on the openweather website, listed by icon ID inside response
+      var weatherIconAlt = (response.list[position].weather[0].description) + " weather icon"; // weather icon alt tag response
+      let temp = `Temperature: ${parseFloat((response.list[position].main.temp - 273.15) * 1.80 + 32).toFixed(2)}°F`; // temperature converted from Kelvin
+      $(this).text(moment(day).format('MMMM DD, YYYY')); // Date display
+      $(this).next().attr('src', weatherIcon); // weather icon display
+      $(this).next().attr('alt', weatherIconAlt); //img alt definition
+      $(this).next().next().text(temp); // Temperature display
+      $(this).next().next().next().text(`Humidity: ${response.list[position].main.humidity}%`); // Humidity
+    });
+
+  });
+}
+
+//Primary search function, triggers on enter/click
+function searchFunction() {
+  citySearch = $('#search').val();
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIkey;
+  var queryForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&cnt=40&appid=" + APIkey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    $('.city').html(`<h2>${response.name} - ${moment().format('MMMM DD, YYYY')}</h2>`);
+    $('.wind').text(`Wind Speed: ${response.wind.speed} MPH`);
+    $('.humidity').text(`Humidity: ${response.main.humidity}%`);
+    let temp = `Tempature: ${parseFloat((response.main.temp - 273.15) * 1.80 + 32).toFixed(2)}°F`;
+    $('.temp').html(temp);
+    lonQuery = (response.coord.lon);
+    latQuery = (response.coord.lat);
+    uvFunction(lonQuery, latQuery);
+    localStorage.setItem('lastCity', `${response.name}`);
   });
 
   function uvFunction(lonQuery, latQuery) {
@@ -70,12 +128,11 @@ function searchFunction() {
   }
 
   createRow();
-  localStorage.setItem('lastCity', citySearch);
-  $('.savedHistory').on('click', historyFunction);
 
+  $('.savedHistory').on('click', historyFunction);
 }
 
-//HISTORY FUNCTION BREAK (must find way to workaround)
+//HISTORY FUNCTION BREAK (must find way to workaround recreating the whole function)
 function historyFunction() {
   var citySearch = $(this).text();
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIkey;
@@ -86,21 +143,18 @@ function historyFunction() {
     method: "GET"
   }).then(function (response) {
 
-    console.log(response);
-
     $('.city').html(`<h2>${response.name} - ${moment().format('MMMM DD, YYYY')}</h2>`);
     $('.wind').text(`Wind Speed: ${response.wind.speed} MPH`);
     $('.humidity').text(`Humidity: ${response.main.humidity}%`);
     let temp = `Tempature: ${parseFloat((response.main.temp - 273.15) * 1.80 + 32).toFixed(2)}°F`;
     $('.temp').html(temp);
-
+    localStorage.setItem('lastCity', `${response.name}`);
   });
 
   $.ajax({
     url: queryForecast,
     method: "GET"
   }).then(function (response) {
-    console.log(response);
 
     $(".fiveday").each(function () {
       var position = $(this).attr('id'); // ID in increments of 8: 8 * 3 = 24 hours (3 hour intervals)
@@ -116,7 +170,6 @@ function historyFunction() {
       $(this).next().next().next().text(`Humidity: ${response.list[position].main.humidity}%`); // Humidity
     });
   });
-  localStorage.setItem('lastCity', citySearch);
 }
 
 $('.buttonInput').on('click', searchFunction);
